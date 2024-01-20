44cb66d57ad6efb183b942e366c62fb034d21fed
shawyuan <mewtwox360@gmail.com>
2024-01-20 16:31:35 +0800

    smart caaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,15 @@
 /*################################變數宣告區################################*/
 // IR sansor A1~A5
 int IR[5] = {A1, A2, A3, A4, A5};
-
 // 速度控制:左邊馬達為PWM5，右邊馬達為PWM6
 int mL = 5;
 int mR = 6;
 // 方向控制:左邊馬達為4，右邊馬達為7
 int sL = 4;
 int sR = 7;
-Servo claw;
+Servo claw; // 爪子伺服馬達
+Servo arm;  // 手臂伺服馬達
+unsigned long duration_time_diff;
 /*################################函數宣告區################################*/
 void motor(int, int);
 void trail();
@@ -23,17 +24,249 @@ void big_turn_left();
 void big_turn_right();
 void forward();
 void stop();
-void IR_test();
-void motor_test();
+void back();
 void re_turn();
 void return_to_line();
 void trail_cross();
 bool cross();
+void my_init();
+void pickup_middle();
+void pickup_left();
+void pickup_right();
+void pick_up();
+void pick_down();
+void arm_up();
+void arm_down();
 /*################################程式初始化################################*/
 void setup() // 程式初始化
 {
-    Serial.begin(9600);
-    Serial.println("hello world");
+    my_init();
+    arm_down();
+    trail_cross();
+
+    pickup_middle(); // 取貨點(中)
+
+    back();
+    delay(500);
+    re_turn();
+    delay(550);
+    return_to_line();
+    delay(100);
+
+    pickup_left(); // 取貨點(左)
+
+    back();
+    delay(500);
+    re_turn();
+    delay(550);
+    return_to_line();
+    delay(100);
+
+    pickup_right(); // 取貨點(右)
+
+    back();
+    delay(500);
+    re_turn();
+    delay(550);
+    return_to_line();
+    delay(100);
+}
+
+/*################################程式循環################################*/
+void loop() // 程式循環
+{
+
+
+    pickup_middle(); // 取貨點(中)
+
+    back();
+    delay(500);
+    re_turn();
+    delay(550);
+    return_to_line();
+    delay(100);
+
+    pickup_left(); // 取貨點(左)
+
+    back();
+    delay(500);
+    re_turn();
+    delay(550);
+    return_to_line();
+    delay(100);
+
+    pickup_right(); // 取貨點(右)
+
+    back();
+    delay(500);
+    re_turn();
+    delay(550);
+    return_to_line();
+    delay(100);
+}
+
+/*################################函數定義區################################*/
+
+void arm_up()
+{
+    arm.write(60); // 調整手臂角度範例
+}
+
+void arm_down()
+{
+    arm.write(90); // 調整手臂角度範例
+}
+
+void pick_up()
+{
+    claw.write(100); // 調整爪子角度
+    arm_up();
+    delay(100);
+}
+
+
+
+void pick_down()
+{
+    arm_down();
+    delay(100);
+    claw.write(20); // 調整爪子角度
+
+}
+
+void pickup_middle()
+{
+    /*################前往第一個取貨點(中)################*/
+    pick_down();
+    // trail_cross(); // 循跡到中間十字路口
+
+    // 繼續前進直到IR1~IR3全部小於450，到達取貨點
+    while (!(analogRead(IR[1]) < 450 and analogRead(IR[2]) < 450 and analogRead(IR[3]) < 450))
+    {
+        trail();
+        if (analogRead(IR[1]) < 450 and analogRead(IR[2]) < 450 and analogRead(IR[3]) < 450)
+        {
+            stop();
+            delay(100);
+        }
+    }
+    stop();
+
+    /*################到達第一個取貨點(中)################*/
+
+    pick_up();
+    arm_up();
+    delay(500);
+
+    /*################前往卸貨點################*/
+    return_to_line(); // 迴轉回到黑線上
+    trail_cross();    // 循跡到中間十字路口
+    trail_cross();    // 循跡到卸貨T字路口
+    stop();
+    /*################到達卸貨點################*/
+
+    pick_down();
+}
+
+void pickup_left()
+{
+    /*################前往第二個取貨點(左)################*/
+    pick_down();
+    // trail_cross();
+    delay(100);
+    while (!((analogRead(IR[1]) > 450)))
+    {
+        big_turn_left();
+    }
+    stop();
+    delay(100);
+    duration_time_diff = millis();
+    while (!(millis() - duration_time_diff >= 500))
+    {
+        trail();
+    }
+    stop();
+    while (!((analogRead(IR[1]) < 450) and (analogRead(IR[2]) < 450) and (analogRead(IR[3]) < 450)))
+    {
+        trail();
+        if (analogRead(IR[1]) < 450 and analogRead(IR[2]) < 450 and analogRead(IR[3]) < 450)
+        {
+            stop();
+            delay(100);
+        }
+    }
+    stop();
+    /*################到達第二個取貨點(左)################*/
+    pick_up();
+    delay(500);
+
+    /*################前往卸貨點################*/
+    return_to_line();
+    trail_cross();
+    big_turn_right();
+    delay(250);
+    while (!(analogRead(IR[4]) > 450))
+    {
+        big_turn_right();
+    }
+    stop();
+    trail_cross(); // 循跡到卸貨T字路口
+    stop();
+    /*################到達卸貨點################*/
+
+    pick_down();
+}
+
+void pickup_right()
+{
+    pick_down();
+    // trail_cross();
+    delay(100);
+    while (!((analogRead(IR[4]) > 450)))
+    {
+        big_turn_right();
+    }
+    stop();
+    delay(100);
+
+    duration_time_diff = millis();
+    while (!(millis() - duration_time_diff >= 500))
+    {
+        trail();
+    }
+    stop();
+    while (!((analogRead(IR[1]) < 450) and (analogRead(IR[2]) < 450) and (analogRead(IR[3]) < 450)))
+    {
+        trail();
+        if (analogRead(IR[1]) < 450 and analogRead(IR[2]) < 450 and analogRead(IR[3]) < 450)
+        {
+            stop();
+            delay(100);
+        }
+    }
+    stop();
+    /*################到達第三個取貨點(右)################*/
+    pick_up();
+    delay(500);
+    /*################前往卸貨點################*/
+    return_to_line();
+    trail_cross();
+    big_turn_left();
+    delay(250);
+    while (!(analogRead(IR[0]) > 450))
+    {
+        big_turn_left();
+    }
+    stop();
+    trail_cross(); // 循跡到卸貨T字路口
+    stop();
+    /*################到達卸貨點################*/
+
+    pick_down();
+}
+
+void my_init()
+{
     for (int i = 0; i < 5; i++)
     {
         pinMode(IR[i], INPUT);
@@ -45,140 +278,8 @@ void setup() // 程式初始化
     pinMode(sR, OUTPUT);
 
     claw.attach(9);
-
-    /*################################程式循環################################*/
-
-    trail_cross(); // 循跡到中間十字路口
-
-    // 繼續前進直到IR1~IR3全部小於450，到達取貨點
-    while (!(analogRead(IR[1]) < 450 && analogRead(IR[2]) < 450 && analogRead(IR[3]) < 450))
-    {
-        trail();
-    }
-    stop();
-
-    return_to_line(); // 迴轉回到黑線上
-    trail_cross();    // 循跡到中間十字路口
-    trail_cross();    // 循跡到卸貨T字路口
-    stop();
-    re_turn();
-    delay(500);
-    return_to_line();
-    trail_cross();
-    delay(100);
-    while (!((analogRead(IR[0]) > 450)))
-    {
-        mid_turn_left();
-    }
-    stop();
-
-    while (!(analogRead(IR[1]) > 450 or analogRead(IR[2]) > 450 or analogRead(IR[3]) > 450))
-    {
-    }
-
-    while (!(analogRead(IR[1]) < 450 && analogRead(IR[2]) < 450 && analogRead(IR[3]) < 450))
-    {
-        trail();
-    }
-    stop();
-
-    return_to_line();
-    trail_cross();
-    mid_turn_right();
-    delay(500);
-    while (!(analogRead(IR[4]) > 450))
-    {
-        big_turn_right();
-    }
-    stop();
-    trail_cross(); // 循跡到卸貨T字路口
-    stop();
-    re_turn();
-    delay(500);
-    return_to_line();
-    trail_cross();
-    delay(100);
-    while (!((analogRead(IR[3]) > 450)))
-    {
-        mid_turn_right();
-    }
-    stop();
-    trail();
-    while (!(analogRead(IR[1]) > 450 or analogRead(IR[2]) > 450 or analogRead(IR[3]) > 450))
-    {
-    }
-
-    while (!(analogRead(IR[1]) < 450 && analogRead(IR[2]) < 450 && analogRead(IR[3]) < 450))
-    {
-        trail();
-    }
-    return_to_line();
-    while (!(analogRead(IR[1]) > 450))
-    {
-        big_turn_left();
-    }
-
-    trail_cross();
-    mid_turn_left();
-    delay(520);
-    while (!(analogRead(IR[1]) > 450))
-    {
-        big_turn_left();
-    }
-    stop();
-    trail_cross(); // 循跡到卸貨T字路口
-
-    stop();
-
-    // forward();
-    // delay(20);
-    // stop();
-    // big_turn_left();
-    // delay(100);
-    // while (!((analogRead(IR[0]) > 450) && (analogRead(IR[4]) < 450)))
-    // {
-    //     big_turn_left();
-    // }
-    // stop();
-    // while (!cross())
-    // {
-    //     trail();
-    // }
-    // // 判斷IR1~IR3有沒有同時小於450，如果有的話就停止
-    // while (!(analogRead(IR[1]) < 450 && analogRead(IR[2]) < 450 && analogRead(IR[3]) < 450))
-    // {
-    //     trail();
-    // }
-    // stop();
-
-    // // 旋轉(big_turn_left)直到IR2大於450，如果有的話就停止
-    // while (!(analogRead(IR[0]) > 450))
-    // {
-    //     big_turn_left();
-    // }
-    // stop();
-    // while (!cross())
-    // {
-    //     trail();
-    // }
-    // stop();
+    arm.attach(10);
 }
-
-/*################################程式循環################################*/
-void loop() // 程式循環
-{
-
-    // claw.write(0);
-    // delay(250);
-    // claw.write(90);
-    // delay(250);
-    // claw.write(180);
-    // delay(250);
-    // claw.write(90);
-    // delay(250);
-}
-
-/*################################函數定義區################################*/
 
 void return_to_line()
 {
@@ -214,7 +315,7 @@ void trail()
         {
             small_turn_right();
         }
-        else if (analogRead(IR[1]) < 450 && analogRead(IR[3]) < 450)
+        else if (analogRead(IR[1]) < 450 and analogRead(IR[3]) < 450)
 
         {
             forward();
@@ -224,17 +325,11 @@ void trail()
     {
         if (analogRead(IR[1]) > 450)
         {
-            // while (!(analogRead(IR[2]) > 450))
-            {
-                mid_turn_left();
-            }
+            mid_turn_left();
         }
         else if (analogRead(IR[3]) > 450)
         {
-            // while (!(analogRead(IR[2]) > 450))
-            {
-                mid_turn_right();
-            }
+            mid_turn_right();
         }
     }
 }
@@ -243,7 +338,7 @@ bool cross()
 {
     // 判斷IR1~IR3有沒有同時大於450，如果有的話就停止
 
-    if (analogRead(IR[1]) > 450 && analogRead(IR[2]) > 450 && analogRead(IR[3]) > 450)
+    if (analogRead(IR[1]) > 450 and analogRead(IR[2]) > 450 and analogRead(IR[3]) > 450)
     {
         stop();
         return true;
@@ -254,13 +349,15 @@ bool cross()
     }
 }
 
-// AND=&&
-// OR=||
 void forward()
 {
     motor(100, 100);
 }
 
+void back()
+{
+    motor(-120, -120);
+}
 void small_turn_left()
 {
     motor(80, 100);
@@ -273,21 +370,21 @@ void small_turn_right()
 
 void mid_turn_left()
 {
-    motor(0, 100);
+    motor(0, 110);
 }
 void mid_turn_right()
 {
-    motor(100, 0);
+    motor(110, 0);
 }
 
 void big_turn_left()
 {
-    motor(-100, 100);
+    motor(-150, 100);
 }
 
 void big_turn_right()
 {
-    motor(100, -100);
+    motor(100, -150);
 }
 
 void stop()
@@ -297,42 +394,15 @@ void stop()
 
 void re_turn()
 {
-    motor(-120, 75);
+    motor(-140, 100);
 }
 
 void motor(int speedL, int speedR)
 {
-
     // 判斷speedL跟speedR正負號決定方向
     digitalWrite(sL, speedL < 0 ? LOW : HIGH); // 左邊馬達方向設定
     digitalWrite(sR, speedR < 0 ? LOW : HIGH); // 右邊馬達方向設定
 
     analogWrite(mL, abs(speedL)); // 左邊馬達速度設定
     analogWrite(mR, abs(speedR)); // 右邊馬達速度設定
-}
-
-void IR_test()
-{
-    for (int i = 0; i < 5; i++)
-    {
-        Serial.print(analogRead(IR[i]));
-        Serial.print(" ");
-    }
-    Serial.println("");
-}
-//
-void motor_test()
-{
-    forward();
-    delay(1000);
-    stop();
-    delay(1000);
-    small_turn_left();
-    delay(1000);
-    small_turn_right();
-    delay(1000);
-    big_turn_left();
-    delay(1000);
-    big_turn_right();
-    delay(1000);
-}
+}   
